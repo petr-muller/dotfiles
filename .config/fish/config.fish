9628d1f5ac0d268e367ca2ce1f3b41dacd009d4c
Petr Muller <afri@afri.cz>
2019-04-15 11:38:33 +0200

    fish config: Add GOPATH setting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,7 @@ end
 
 source /usr/share/autojump/autojump.fish
 
+set -gx GOPATH $HOME/Projects/RH/go
+
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/afri/Applications/google-cloud-sdk/path.fish.inc' ]; . '/home/afri/Applications/google-cloud-sdk/path.fish.inc'; end
