bd9b454f3f2d344bfd4b79e58f007828a42362c7
Petr Muller <afri@afri.cz>
2019-05-07 11:41:35 +0200

    Add GOPATH/bin to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ end
 source /usr/share/autojump/autojump.fish
 
 set -gx GOPATH $HOME/Projects/RH/go
+set -gx PATH $PATH $GOPATH/bin
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/afri/Applications/google-cloud-sdk/path.fish.inc' ]; . '/home/afri/Applications/google-cloud-sdk/path.fish.inc'; end
