a29ac4b620e58639b1f028dc7aec6973991bf6fc
Petr Muller <afri@afri.cz>
2020-02-10 14:38:03 +0100

    Add /home/remote/pmuller/Applications/bin to /home/remote/pmuller/Applications/google-cloud-sdk/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/remote/pmuller/Projects/RH/go/bin:/home/remote/pmuller/Applications/bin:/home/remote/pmuller/Projects/RH/go/bin:/home/remote/pmuller/Applications/bin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ source /usr/share/autojump/autojump.fish
 
 set -gx GOPATH $HOME/Projects/RH/go
 set -gx PATH $PATH $GOPATH/bin
+set -gx PATH $PATH $HOME/Applications/bin
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/Applications/google-cloud-sdk/path.fish.inc" ]; . "$HOME/Applications/google-cloud-sdk/path.fish.inc"; end
