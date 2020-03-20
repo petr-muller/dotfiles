e1fe2d5120c6c4fa612447000d113389188fdeae
Petr Muller <afri@afri.cz>
2020-03-20 12:49:55 +0100

    Add a function to watch PJs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,25 @@ function fish_prompt
     powerline-shell --shell bare $status
 end
 
+function pj-watch
+  set PJ $argv[1]
+  if [ -z $PJ ]
+    echo "Needs a Prowjob ID"
+    return
+  end
+  while [ -z $POD ]
+     echo "Waiting for pod to be assigned for Prowjob"
+     set POD (oc get pj $PJ -o jsonpath='{.status.pod_name}')
+     sleep 5
+  end
+  echo "Pod: $POD"
+  while ! oc logs $POD -c test -f
+     echo "Waiting for pod to run"
+     echo "Status: "(oc get pod $POD -o jsonpath={.status.phase})
+     sleep 5
+  end
+end
+
 source /usr/share/autojump/autojump.fish
 
 set -gx GOPATH $HOME/Projects/RH/go
